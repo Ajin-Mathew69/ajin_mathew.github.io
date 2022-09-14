f11552849273b8734ebc35a6be7472a47144bc7c
Ajin-Mathew69 <ajin.mathewfsd@gmail.com>
2022-09-14 17:16:28 +0530

    Completed Servies and Proect section & Added swiper function

--- assets/JAVASCRIPT/script.js
+++ assets/JAVASCRIPT/script.js
@@ -57,3 +57,52 @@ tabs.forEach((tab) => {
         tab.classList.add("qualification__active");
     });
 });
+/* ========= SERVICES MODAL ================ */
+const modalViews = document.querySelectorAll(".services__modal"),
+    modalButtons = document.querySelectorAll(".services__button"),
+    modalCloses = document.querySelectorAll(".services__modal-close");
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add("active-modal");
+};
+modalButtons.forEach((modalBtn, i) => {
+    modalBtn.addEventListener("click", () => {
+        modal(i);
+    });
+});
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener("click", () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove("active-modal");
+        });
+    });
+});
+/**==== PORTFOLIO SWIPER ============= */
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+/*========== TESTIMONIAL ============== */
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        },
+    },
+});
